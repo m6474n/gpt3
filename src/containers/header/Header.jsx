3053196dad0c65b298c6aa04d238e7da22beea7c
Muhammad Mohsin <getupfools@gmail.com>
2023-07-21 00:37:27 +0500

    Header Section Updated

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -18,15 +18,15 @@ const Header = () => {
         </p>
         <div className="gpt3__header-content_input">
           <input type="email" placeholder="Your Email Address" />
-          <button type="button">Submit</button>
+          <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-context_people">
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        {/* <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div> */}
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
