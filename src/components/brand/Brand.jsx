edf09e9c5fdaa6f550449ff03042eeef6e26f87b
Muhammad Mohsin <getupfools@gmail.com>
2023-07-21 23:12:57 +0500

    what is gpt3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./brand.css";
-
+import { google, slack, dropbox, atlassian, shopify } from "./imports.js";
 const Brand = () => {
-  return <div>Brand</div>;
+  return (
+    <div className="gpt3__brands section__padding">
+      <div>
+        <img src={google} alt="google" srcset="" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" srcset="" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassian" srcset="" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" srcset="" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" srcset="" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__feature-container_feature">
+      <div className="gpt3__feature-conteiner_feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__feature-container_feature-text">{text}</div>
+    </div>
+  );
 };
 
 export default Feature;
